8501c410d60401daf51dd6209079b4dd991f7072
Bola Muyis <bolamuyis@gmail.com>
2018-12-17 15:57:49 -0500

    fix git conflicts

--- genesect.js
+++ genesect.js
@@ -7,22 +7,12 @@ axios.get("https://pokeapi.co/api/v2/pokemon/649/")
     const data = response.data;
     console.log(data); 
 
-<<<<<<< HEAD
 // Capture all the indexes of abilities in to this array
 var abilitiesArray = [];
 for (let i=0; i<data.abilities.length; i++){
     abilitiesArray.push(data.abilities[i].ability.name);
 } 
 
-=======
-//declare and empty array to capture all the abilities
-var abilitiesArray = [];
-
-// Create a loop that brings in all the abilities data
-for (let i=0; i<data.abilities.length; i++){
-    abilitiesArray.push(data.abilities[i].ability.name);
-}
->>>>>>> master
 let electivire = new Pokemon(
     data.forms[0].name,
     //data.abilities[0].ability.name,
